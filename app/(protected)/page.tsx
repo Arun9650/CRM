ddc554e182bd53eb5f132f53bef9bcc85d54d049
Arun9650 <arun02580@gmail.com>
2024-10-03 01:03:02 +0530

    Refactor protected page component to remove unused imports and update code formatting

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -1,20 +1,10 @@
-'use client';
 import UserTable from '@/components/UserTable'
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import React, { useEffect } from 'react'
+import React from 'react'
 
 
 const Home = () => {
 
-  const router = useRouter()
-  const session = useSession();
-
-  useEffect(() => {
-    if (session.status === 'unauthenticated') {
-      router.push('/sign-in');
-    }
-  },[session])
+  
 
   return (
     <UserTable/>
